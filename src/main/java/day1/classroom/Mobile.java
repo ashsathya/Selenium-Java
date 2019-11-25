75de01ad5ef23b7397cd6de12f51481c90fbe6ec
Ash <Ash@DESKTOP-OP45372>
2019-11-25 18:15:57 +0530

    Week 1 Classroom

---
+++ src/main/java/day1/classroom/Mobile.java
@@ -0,0 +1,42 @@
+package day1.classroom;
+
+public class Mobile {
+
+	public void makecall(String name) {
+		System.out.println("Make call to " + name);
+	}
+
+	public void sendsms(String textmessage) {
+		System.out.println("Send sms to sathya " + textmessage);
+
+	}
+
+	public boolean sendsms() 
+	{
+		System.out.println("Send sms to sathya");
+		return true;
+
+	}
+
+	public boolean shutDown()
+	{
+		System.out.println("Mobile is shutdown");
+		return true;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mobile phone = new Mobile();
+
+		String name = "Sathya";
+		phone.makecall(name);
+
+		String textmessage = "Learning selenium";
+		phone.sendsms();
+		phone.sendsms(textmessage);
+
+		phone.shutDown();
+
+	}
+
+}
